d6b4dadeb1d6cf4388e1ffed6e3b938d74d9c8d3
Andreas Åkerlöf <andreas@akerlof.se>
2019-09-17 19:11:59 +0200

    Skills påbörjat

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../components/layout";
 import Home from "../components/index/home";
+import Skills from "../components/index/skills";
 import About from "../components/index/about";
 import Portfolio from "../components/index/portfolio";
 import Contact from "../components/index/contact";
@@ -9,7 +10,7 @@ export default () => (
   <Layout>
     <Home parentId="home" />
     <About parentId="about" />
-    {/* <Skills parentId="skills" />> */}
+    <Skills parentId="skills" />
     <Portfolio parentId="portfolio" />
     <Contact parentId="contact" />
   </Layout>
