a3a750c0fac18c657da7c836f5f406f40cb8c8c7
Andreas Åkerlöf <andreas@akerlof.se>
2019-09-19 19:15:28 +0200

    Flyttat CSS från global

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,12 @@ import Skills from "../components/index/skills";
 import About from "../components/index/about";
 import Portfolio from "../components/index/portfolio";
 import Contact from "../components/index/contact";
+import stylesHome from "../styles/home.module.scss";
 import stylesAbout from "../styles/about.module.scss";
 
 export default () => (
   <Layout>
-    <Home parentId="home" />
+    <Home parentId="home" parentClassName={stylesHome.parent} />
     <About parentId="about" parentClassName={stylesAbout.parent} />
     <Skills parentId="skills" />
     <Portfolio parentId="portfolio" />
--- src/pages/submit.js
+++ src/pages/submit.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Layout from "../components/layout";
 import ThankYou from "../components/submit/thankyou";
+import stylesThankYou from "../styles/thankyou.module.scss";
 
 export default () => (
   <Layout>
-    <ThankYou parentId="thankyou" />
+    <ThankYou parentId="thankyou" parentClassName={stylesThankYou.parent} />
   </Layout>
 );
