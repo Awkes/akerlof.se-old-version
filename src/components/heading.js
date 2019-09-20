45fbefeb656bbd07055afd9ce49dc73906c38937
Andreas Åkerlöf <andreas@akerlof.se>
2019-09-20 10:19:52 +0200

    Animerat headings

--- src/components/heading.js
+++ src/components/heading.js
@@ -1,5 +1,13 @@
 import React from "react";
+import VisibilitySensor from "react-visibility-sensor";
+import styles from "../styles/heading.module.scss";
 
 export default({children}) => (
-  <h1><span className={'underlined'}>{children}</span></h1>
+  <VisibilitySensor>
+    {({isVisible}) => (
+      <h1 className={`${styles.heading} ${isVisible ? styles.active : ''}`}>
+        <span className={styles.underlined}>{children}</span>
+      </h1>
+    )}
+  </VisibilitySensor>
 );
